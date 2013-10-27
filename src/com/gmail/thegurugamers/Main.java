f0c28cc1d10d936d3e06843a42b878ba91f897f7
Daniel <thegurugamers@gmail.com>
2013-10-26 22:45:34 -0400

    Copyright header

--- src/com/gmail/thegurugamers/Main.java
+++ src/com/gmail/thegurugamers/Main.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2013 Daniel S. and all contributors
+ * Do not redistribute any of the code in this project without
+ * explicitly asking the owner first
+ * 
+ */
+
 package com.gmail.thegurugamers;
 
 import org.bukkit.plugin.java.JavaPlugin;

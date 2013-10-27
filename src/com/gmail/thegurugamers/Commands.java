adbb5b5dd221c5632fa3c55679dffacc9c0783d4
Daniel <thegurugamers@gmail.com>
2013-10-26 22:06:47 -0400

    Bug fixes & code organization

--- src/com/gmail/thegurugamers/Commands.java
+++ src/com/gmail/thegurugamers/Commands.java
@@ -19,61 +19,52 @@ public class Commands implements CommandExecutor {
 		ItemMeta itemMeta = item.getItemMeta();
 		
 		StringBuilder str = new StringBuilder();
-	      for (int i = 0; i < args.length; i++){
-	      str.append(args[i] + " ");
-	      }
-	      
-	      String dname = str.toString();
-	     //String lname = str.toString();
-	      
-	      
-	      if (!(item.getType() == Material.AIR)){
-	    	  itemMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', dname));
-              //itemMeta.setLore(Arrays.asList(ChatColor.translateAlternateColorCodes('&', lname)));
-              itemMeta.setLore(Arrays.asList(ChatColor.GREEN + "Owner: " + ChatColor.RED + p.getName()));
-              item.setDurability(item.getDurability());
-              
-	      }
-	      
-	      if (commandLabel.equalsIgnoreCase("rename")) {
-	    	  if (p.hasPermission("renamer.rename")){
-	    		  if (args.length > 0){
-					if (sender instanceof Player) {
-						if (!(item.getType() == Material.AIR)){
-							p.getInventory().removeItem(p.getInventory().getItemInHand());
-							p.sendMessage(ChatColor.AQUA + "You renamed your " + ChatColor.RED + 
-								item.getType() + ChatColor.AQUA + " to " + ChatColor.WHITE + ChatColor.translateAlternateColorCodes('&', dname)); 
-							item.setItemMeta(itemMeta);
-							p.getInventory().addItem(item);
-					
-						}else{
-							p.sendMessage(ChatColor.DARK_RED + "You must have an item in hand to do that!");
-						}
-				
-					}else{
-						sender.sendMessage(ChatColor.DARK_RED + "Must be a player to use that command!");
-					}
-				}else{
-					sender.sendMessage(ChatColor.DARK_RED + "Not enough arguments! Try /rename <name>");
-				}
-				
-			}else{
-				sender.sendMessage(ChatColor.DARK_RED + "You don't have permission to rename that item :(");
-			}
+		for (int i = 1; i < args.length; i++){
+			str.append(args[i] + " ");
 		}
-		
-		/*if (commandLabel.equalsIgnoreCase("setlore")){
-			if (args.length > 0){
-				if (sender instanceof Player){
-					if (!(item == null)){
-						item.setItemMeta(itemMeta);
-						
+	      
+		String dname = str.toString();
+		//String lname = str.toString();
+	      
+	      
+		if (!(item.getType() == Material.AIR)){
+			itemMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', dname));
+			//itemMeta.setLore(Arrays.asList(ChatColor.translateAlternateColorCodes('&', lname)));
+			itemMeta.setLore(Arrays.asList(ChatColor.GREEN + "Owner: " + ChatColor.RED + p.getName()));
+			item.setDurability(item.getDurability());
+              
+		}
+	      
+		if (sender instanceof Player){
+			if (commandLabel.equalsIgnoreCase("item")) {
+				if (p.hasPermission("renamer.rename")){
+					if (args.length > 1){
+						if (args[0].equalsIgnoreCase("rename")){
+							if (!(item.getType() == Material.AIR)){
+								p.getInventory().removeItem(p.getInventory().getItemInHand());
+	    				  		p.sendMessage(ChatColor.AQUA + "You renamed your " + ChatColor.RED + 
+	    				  				item.getType() + ChatColor.AQUA + " to " + ChatColor.WHITE + ChatColor.translateAlternateColorCodes('&', dname)); 
+	    				  		item.setItemMeta(itemMeta);
+	    				  		p.getInventory().addItem(item);
+					
+							}else{
+								p.sendMessage(ChatColor.DARK_RED + "You must have an item in hand to do that!");
+							}
+						}
+	    			  
+					}else{
+						sender.sendMessage(ChatColor.DARK_RED + "Not enough arguments! Try /rename <name>");
 					}
+	    		  
+				}else{
+					sender.sendMessage(ChatColor.DARK_RED + "You don't have permission to rename your item :(");
 				}
 			}
-		}*/
-		
+	    	  
+		}else{
+			sender.sendMessage(ChatColor.DARK_RED + "You must be a player in order to use that command!");
+		}
+	      
 		return false;
 	}		
-
 }
--- src/com/gmail/thegurugamers/Main.java
+++ src/com/gmail/thegurugamers/Main.java
@@ -5,7 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
 	
 	public void onEnable(){
-		getCommand("rename").setExecutor(new Commands());
+		getCommand("item").setExecutor(new Commands());
 	}
 	
 	public void onDisable(){
